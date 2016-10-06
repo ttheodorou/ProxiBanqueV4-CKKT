03d9d3913b5d02d8c2ce5e69175dc509ac1c2d84
Konrad Thomas <konrad.thomas@aol.com>
2016-10-06 17:09:34 +0200

    Dernier commit pour la journée

--- ProxiBanqueV4/ProxiBanqueV4-service/src/main/java/com/proxibanquev4/ckkt/service/CompteService.java
+++ ProxiBanqueV4/ProxiBanqueV4-service/src/main/java/com/proxibanquev4/ckkt/service/CompteService.java
@@ -2,11 +2,17 @@ package com.proxibanquev4.ckkt.service;
 
 public class CompteService {
 	
+//	CompteDAO compteDAO = new CompteDAO();
+	
 	public boolean virement(long noCompteADebiter, long noCompteACrediter, double montant) {
-		return false;
+		debiter(noCompteADebiter, montant);
+		crediter(noCompteACrediter, montant);
+		return true;
 	}
 	
 	public boolean crediter (long noCompteACrediter, double montant) {
+//		CompteBancaire compte = lireCompte(noCompteACrediter);
+//		updateSolde(noCompteACrediter, compte.getSolde() - montant);
 		return false;
 	}
 
