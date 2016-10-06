cfc710b32d75e53c3f6ce16e3d05ef6665874f9e
Clement <castro.clement@gmail.com>
2016-10-06 12:35:25 +0200

    création de la classe Abstraite Personne.

---
+++ ProxiBanqueV4/ProxiBanqueV4-domaine/src/main/java/com/proxibanquev4/ckkt/domaine/Personne.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.proxibanquev4.ckkt.domaine;
+
+/**
+ * Classe Personne.java : Represente des personnes de ProxiBanque.
+ * @author CKKT
+ */
+public abstract class Personne {
+
+}
